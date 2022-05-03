857a09406e4ee3fb163e43d1589d680d9b8c8868
Belen Castillo <pcastillomorales19z2gmail.com>
2022-05-03 12:04:02 -0500

    Maquetación en html

--- paginaweb/js/index.js
+++ paginaweb/js/index.js
@@ -1,21 +1,16 @@
 var url ='https://api.nasa.gov/planetary/apod'
 var key ='?api_key=XpOvDkdTN0TLcI8oLVWMe6IzSeSgatdiYiITV5Nq'
-var date = '?date=2022-02-11'
 var urlWithKey = url+key
-var urlWithDate = url+date
-
-fetch(urlWithKey)
-.then(response => {
-  console.log(response.json())
-})
-
-fetch(urlWithDate)
-.then(response => {
-  console.log(response.json())
-})
 
 var sign = prompt("Hola!, ingresa una fecha");
+var date = '&date='+sign
+var urlWithSign = urlWithKey+date
 
 if (sign.toLowerCase() == "2022-05-03") {
   alert(urlWithDate);
 }
+
+fetch(urlWithSign)
+.then(response => {
+  console.log(response.json())
+})
